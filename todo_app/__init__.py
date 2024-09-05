417e36eba0771739e5fd9872a20eab4f1760a9cf
Jens <jltk@pm.me>
2024-09-05 17:27:32 +0200

    Update __init__.py
    Add metadata

---
+++ todo_app/__init__.py
@@ -0,0 +1,9 @@
+from todo_app import TodoApp
+
+__all__ = ['TodoApp']
+
+__version__ = '0.2.0'
+__author__ = 'Jens Lettkemann'
+__email__ = 'jltk@pm.me'
+__license__ = 'GPLv3+'
+__description__ = 'A simple Todo application with a graphical user interface.'
