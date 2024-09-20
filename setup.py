5387fc41fa341b67c390092f1b34ecc9a2f9cfb7
Jens <jltk@pm.me>
2024-09-21 01:51:02 +0200

    Update setup.py
    0.3.0 update

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
-from setuptools import setup, find_packages
+import sys
+from cx_Freeze import setup, Executable
+
+build_exe_options = {
+    "packages": ["tkinter", "json", "pathlib"],
+    "excludes": ["unittest", "email", "html", "http", "xml"],
+    "include_files": [
+        ("todo_app/app_icon.ico", "app_icon.ico"),
+        ("todo_app/app_logo.png", "app_logo.png"),
+    ],
+}
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
 
 setup(
-    name="todo-app",
-    version="0.2.2",
-    packages=find_packages(),
-    install_requires=[
-
-    ],
-    entry_points={
-        'console_scripts': [
-            'todo-app=todo_app.app:main',
-        ],
-    },
+    name="TodoApp",
+    version="0.3.0",
+    description="A simple Todo application with python Tkinter GUI",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("todo_app/todo_app.py", base=base, icon="todo_app/app_icon.ico")],
 )
